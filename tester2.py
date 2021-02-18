2f9fced5f189311ccd402934a5597810ccf91816
1darpan <62384187+1darpan@users.noreply.github.com>
2021-02-18 16:12:13 +0530

    Change
    change

--- tester2.py
+++ tester2.py
@@ -4,46 +4,42 @@ import os.path
 import numpy as np
 import faceRecognition as fr
 
-name={0:"Darpan",1:"Kangana",2:"Amol",3:"Dipen",4:"Akshay"}
 
-def trainModel():
-   test = os.path.exists('trainingData.yml')
+path_of_model = 'C:\\Users\darpa\Documents\GitHub\Face-Recognition-using-Open-CV-/trainingData.yml'
+
+l1 = [{0:'Darpan','image':'C:\\Users\darpa\Documents\GitHub\Face-Recognition-using-Open-CV-\trainingImages\0'},{1:'Dipen','image':'C:\\Users\darpa\Documents\GitHub\Face-Recognition-using-Open-CV-\trainingImages\1'},{2:'Amol','image':'C:\\Users\darpa\Documents\GitHub\Face-Recognition-using-Open-CV-\trainingImages\2'},{4:'Akshay','image':'C:\\Users\darpa\Documents\GitHub\Face-Recognition-using-Open-CV-\trainingImages\3'}]
+
+def train(absolutePathModel,list):
+   test = os.path.exists(path_of_model)
    if(not test):
        faces,faceID=fr.labels_for_training_data('trainingImages')
        face_recognizer=fr.train_classifier(faces,faceID)
        face_recognizer.write('trainingData.yml')
-   else:
-        face_recognizer=cv2.face.LBPHFaceRecognizer_create()
-        face_recognizer.read('trainingData.yml')
-   return face_recognizer
+#    else:
+#         face_recognizer=cv2.face.LBPHFaceRecognizer_create()
+#         face_recognizer.read('trainingData.yml')
 
-face_recognizer = trainModel()
 
-def identify(imagePath):
+def identify( imagePath , modelPath):
     dict={}
+    recognizerModel=cv2.face.LBPHFaceRecognizer_create()
+    recognizerModel.read(modelPath)
     test_img = cv2.imread(imagePath)
     faces_detected,gray_img=fr.faceDetection(test_img)
     for face in faces_detected:
         (x,y,w,h)=face
         roi_gray=gray_img[y:y+h,x:x+h]
-        label,confidence=face_recognizer.predict(roi_gray)#predicting the label of given image
+        label,confidence=recognizerModel.predict(roi_gray)#predicting the label of given image
         dict['Confidence']=confidence
-        dict['Label']=name[label]
-        print(dict)
-        fr.draw_rect(test_img,face)
-        predicted_name=name[label]
-        if(confidence>50):#If confidence more than 37 then don't print predicted face text on screen
-            continue
-        fr.put_text(test_img,predicted_name,x,y)
+        dict['Label']=l1[label][label]
+        return dict
 
-testImage = 'TestImages/Darpan.jpg'    
+        
 
-identify('TestImages/Darpan.jpg')
+train(path_of_model,l1)
 
-resized_img=cv2.resize(testImage,(1000,1000))
-cv2.imshow("Face detection",resized_img)
-cv2.waitKey(0)#Waits indefinitely until a key is pressed
-cv2.destroyAllWindows
+Results = identify('TestImages/Darpan.jpg',path_of_model)
 
+print(Results)
 
 
